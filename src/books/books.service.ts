c722f1c59e5609bc2299cebfafda739196ca1fb4
injuk <ingnoh1001@gmail.com>
2023-02-07 00:16:59 +0900

    feature: Book 생성 API에 트랜잭션 도입

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,32 +1,41 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Logger, Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateBookDto } from './dto/create-book.dto';
-import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class BooksService {
   private readonly logger = new Logger(BooksService.name);
+
   constructor(private readonly prisma: PrismaService) {}
 
   createBook(dto: CreateBookDto) {
-    // TODO: 트랜잭션 안에서 author, isbn 검색 후 생성 시도할 것
-    this.logger.debug('create book, ', JSON.stringify(dto));
+    const { author, isbn } = dto;
 
-    const data: Prisma.BookCreateInput = {
-      title: dto.title,
-      originalTitle: dto.originalTitle,
-      author: dto.author,
-      publisher: dto.publisher,
-      publishedAt: dto.publishedAt,
-      isbn: dto.isbn,
-      genre: dto.genre,
-      coverUrl: dto.coverUrl,
-      createdBy: 1,
-      updatedBy: 1,
-    };
+    return this.prisma.$transaction(async (transactionCtx) => {
+      const book = await transactionCtx.book.findUnique({
+        where: {
+          UQ_AUTHOR_ISBN: { author, isbn },
+        },
+      });
 
-    return this.prisma.book.create({
-      data,
+      if (book) throw new ConflictException(`author(${author}), isbn(${isbn})`);
+
+      return transactionCtx.book.create({
+        data: {
+          title: dto.title,
+          originalTitle: dto.originalTitle,
+          author: dto.author,
+          publisher: dto.publisher,
+          publishedAt: dto.publishedAt,
+          isbn: dto.isbn,
+          genre: dto.genre,
+          coverUrl: dto.coverUrl,
+
+          // TODO: users API가 도입되면 수정할 것
+          createdBy: 1,
+          updatedBy: 1,
+        },
+      });
     });
   }
 }
