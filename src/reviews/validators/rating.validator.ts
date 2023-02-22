bedd3be3cd9ace3bd01a43b436f003f8c396c4af
injuk <ingnoh1001@gmail.com>
2023-02-23 00:45:58 +0900

    db: rating을 float으로 수정

--- src/reviews/validators/rating.validator.ts
+++ src/reviews/validators/rating.validator.ts
@@ -8,10 +8,10 @@ import {
 export class ValidRating implements ValidatorConstraintInterface {
   private readonly min = 0;
   private readonly max = 5;
-  private readonly condition = 0.5;
+  private readonly condition = 1;
 
   validate(rating: number, validationArguments: ValidationArguments) {
     if (rating < this.min || rating > this.max) return false;
-    return rating === 0 || Math.abs(rating % this.condition) === 0;
+    return rating === 0 || Math.abs((rating * 10) % this.condition) === 0;
   }
 }
