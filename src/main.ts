caccd953dc33f2ff3cd8716b8037d2c9be541458
injuk <ingnoh1001@gmail.com>
2023-05-10 11:42:09 +0900

    feature: 애플리케이션 CORS 활성화

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import commonConfig from 'src/config/common.config';
 import { ConfigType } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config: ConfigType<typeof commonConfig> = app.get(commonConfig.KEY);
 
   await app.listen(config.application.port ?? 3000);
